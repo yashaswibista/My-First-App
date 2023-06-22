588735576bb84c7987dcabab65279e16ebbb2fde
yashaswibista <122423954+yashaswibista@users.noreply.github.com>
2023-06-22 12:19:05 -0600

    Initial Commit

---
+++ My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by Yashaswi Bista on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
