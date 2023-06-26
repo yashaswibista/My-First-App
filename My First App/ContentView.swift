014e7902c694cc194c38197cd770f2923790cf02
yashaswibista <122423954+yashaswibista@users.noreply.github.com>
2023-06-26 11:46:34 -0600

    I changed the color and name

--- My First App/ContentView.swift
+++ My First App/ContentView.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("What is your name?")
+            TextField("Type...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.purple, width: 1)
+            Button("Submit Name") {
+                textTitle = "We"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            Text("ii")
         }
         .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
